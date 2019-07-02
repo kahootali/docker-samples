4276b3db0165aad785d82f453792d262288869ce
alikahoot2 <kahoot.ali@gmail.com>
2019-07-03 00:58:54 +0500

    add python app

---
+++ python-app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+count = 0
+
+@app.route('/hello')
+def hello():
+    greeting = "Hello world!"
+    return greeting
+
+@app.route('/count', methods=["GET"])
+def getCount():
+    global count
+    count += 1
+    return str(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080)
