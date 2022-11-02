f1c14e647605b30741d5d22b4c3153d854c9591e
kahootali <kahoot.ali@gmail.com>
2022-11-03 01:19:08 +0500

    update students list for batch10

--- microservices/backend-python/app.py
+++ microservices/backend-python/app.py
@@ -3,16 +3,16 @@ app = Flask(__name__)
 
 instructors = [
     { 'firstName': "Muhammad Ali", 'lastName': "Kahoot"  },
-    { 'firstName': "Hasnain", 'lastName': "Zaib"  }
+    { 'firstName': "Mehmood", 'lastName': "Khan"  }
 ]
 students = [
-    { 'id': "1", 'firstName': "Abdul", 'lastName': "Raheem"  },
-    { 'id': "2",'firstName': "Sarim", 'lastName': "Hameed"  },
-    { 'id': "3",'firstName': "Obaid", 'lastName': "Azizi"  },
-    { 'id': "4",'firstName': "Fahad", 'lastName': "Maqsood"  },
+    { 'id': "1", 'firstName': "Hassan", 'lastName': "Sheikh"  },
+    { 'id': "2",'firstName': "Kazim", 'lastName': "Ali"  },
+    { 'id': "3",'firstName': "Adeel", 'lastName': "Ahmed"  },
+    { 'id': "4",'firstName': "Adeel", 'lastName': "Raza"  },
     { 'id': "5",'firstName': "Test", 'lastName': "User"  },
-    { 'id': "6",'firstName': "Bilal", 'lastName': "Butt"  },
-    { 'id': "7",'firstName': "Ch", 'lastName': "Usama"  }
+    { 'id': "6",'firstName': "Ali", 'lastName': "Ahmed"  },
+    { 'id': "7",'firstName': "Hamad", 'lastName': "Khan"  }
 ]
 
 @app.route('/hello')
